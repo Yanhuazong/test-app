38453590bfb146ba93283eb13cdcd760b071c7d7
Hannah Yanhua Zong <zong6@purdue.edu>
2025-01-23 17:41:58 -0500

    Add props and state variable

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,54 @@
 import "./App.css";
 import About from "./components/About";
 import Navbar from "./components/Navbar";
-import Card1 from "./components/Card1";
-import Card2 from "./components/Card2";
+import image_man from "./assets/headshot-man.png";
+import image_woman from "./assets/headshot-woman.png";
+import Card from "./components/Card";
+import Wrapper from "./components/Wrapper";
+import { useState } from "react";
 
 const App = () => {
+  const profiles = [
+    {
+      img: image_man,
+      name: "John Doe",
+      title: "Software Engineer",
+      email: "a@a.com",
+    },
+    {
+      img: image_woman,
+      name: "Lily Smith",
+      title: "UX designer",
+      email: "b@a.com",
+    },
+  ];
+  const [clicked, setClicked] = useState(false);
+  const handleClick = () => {
+    setClicked(!clicked);
+  };
+
   return (
     <>
       <header>
         <Navbar />
       </header>
       <main>
-        <div className="section">
-          <div className="container">
-             <h1>Profile App</h1>
+        <Wrapper>
+          <h1>Profile App</h1>
+          <button onClick={handleClick}>
+            {clicked ? "Click me" : "Clicked"}
+          </button>
+        </Wrapper>
+        <Wrapper>
+          <About />
+        </Wrapper>
+        <Wrapper>
+          <div className="profile-cards">
+            {profiles.map((profile) => (
+              <Card key={profile.email} {...profile} />
+            ))}
           </div>
-        </div>
-        <div className="section">
-          <div className="container">
-             <About />
-          </div>
-        </div>
-        <div className="section">
-          <div className="container">
-             <div className="profile-cards">
-                <Card1 />
-                <Card2 />
-              </div>
-          </div>
-        </div>
+        </Wrapper>
       </main>
     </>
   );
