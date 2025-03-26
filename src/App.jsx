72b13b8bea8da48178c272e90e2e1cbbec81d81b
Hannah Zong <yanhuazong@gmail.com>
2025-03-25 23:24:22 -0400

    Performance optimization examples

--- src/App.jsx
+++ src/App.jsx
@@ -11,14 +11,14 @@ import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage"; 
 import { HashRouter, Routes, Route } from "react-router-dom";
 import { useMode } from "./contexts/ModeContext"; 
-import { useContext } from "react";
+import { lazy, Suspense } from "react";
 import { AuthProvider } from "./contexts/AuthContext";
 import ProtectedRoute from "./components/ProtectedRoute";
 
 const App = () => {
 
   const { mode } = useMode();
-
+  const LazyComponent = lazy(() => import("./pages/ProfileDetailPage"));
   return (
     <AuthProvider>
       <HashRouter>
@@ -35,7 +35,7 @@ const App = () => {
               </ProtectedRoute>
               } />
             <Route path="/profile/:id" element={<ProfileIndexPage />}>
-              <Route index element={<ProfileDetailPage />} />
+              <Route index element={<Suspense fallback = {<div>Loading...</div>}><LazyComponent /></Suspense>} />
               <Route path="edit" element={<ProtectedRoute><ProfileEditPage /></ProtectedRoute>} />
             </Route>
             <Route path="/login" element={<LoginPage />} />
